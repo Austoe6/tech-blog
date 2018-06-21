f20639872b8d1731b04f75284168482274e516ad
Austin Okhala <37802279+Austoe6@users.noreply.github.com>
2018-06-21 23:45:40 +0300

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,19 @@ module.exports = {
       },
     },
     'gatsby-plugin-netlify-cms',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-120919873-3",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
